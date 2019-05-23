246201b0e44a8f6f66353f1ea300f190afe85324
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2019-05-23 00:24:10 -0300

    ola

--- public/js/controllers/fotos-controllers.js
+++ public/js/controllers/fotos-controllers.js
@@ -1,7 +1,17 @@
 angular.module('alurapic').
 		controller('FotosController', function($scope){
-			$scope.foto = {
+			$scope.fotos = [
+			{
 				titulo: 'leao',
 				url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-			};
+			},
+			{
+				titulo: 'leao 2',
+				url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+			},
+			{
+				titulo: 'leao 3',
+				url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+			}
+			];
 		});
